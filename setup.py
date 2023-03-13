7e7edc72cbeff4ad6aeaa61076d9c730fe4c5aa9
SarshenR <sarshenr@hollywoodbets.com>
2023-03-13 15:23:10 +0200

    commit

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='packageOwnPay',
+    version='0.0.1',
+    author='Sarshen Ramsamy',
+    author_email='sarshenr@hollywoodbets.net',
+    description='Testing installation of Package',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/SarshenR/Python',
+    project_urls = {
+        "Bug Tracker": "https://github.com/SarshenR/Python"
+    },
+    license='MIT',
+    packages=['toolbox'],
+    install_requires=['requests'],
+)
